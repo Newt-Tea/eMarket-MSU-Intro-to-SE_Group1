b9a0a6617b55af36826dc9495edfefa89456350d
Hudson H <Hudsonhargrove01@gmail.com>
2024-10-20 14:16:16 -0500

    Implemented Account Creation Functionailty

--- eMarket/app/urls.py
+++ eMarket/app/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
   path('', views.product_list, name='product_list'),
+  #path('product/<int:product_id>/', views.product_detail, name='product_detail'),
   path('', views.login, name='login'),
-  path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+  path('register/', views.register, name='register'),
+  path('registration_success/', views.registration_success, name='registration_success'),
+  path('logout/', views.logout_view, name='logout'),
 ]
