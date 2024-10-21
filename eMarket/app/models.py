48fd024960b76c850372675ca3a9580625919193
LostSpaghetti <mccrosno@gmail.com>
2024-10-21 13:30:11 -0500

    Added updated models.py file
    Fixed bug with cart model and added on_delete paramaters to other ForeignKeys. Additionally, added max_digits to DecimalField

--- eMarket/app/models.py
+++ eMarket/app/models.py
@@ -5,37 +5,43 @@ from django.contrib.auth.models import AbstractUser
 # the databases are complete & migrated. just placeholders RN ** do Not make migrations yet**
 
 class User(AbstractUser):
-    accountBalance = models.IntegerField()
+
+    #accountBalance = models.IntegerField() # Possibly unneeded, as we will not need to have an account balance
+    userID = -1 # User ID is non-functional until we can reference the length of the user database
+    """"
     user_types = [
         'admin',
         'buyer',
         'seller',
     ]
+    user_type = models.CharField(max_length=20, choices=user_types)
+    """
 
 # i will change this functionality, just gotta think about this *strokes chin*
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=6, choices=user_types)
-    class Admin:
-        def is_admin(self):
-            return self.user_type == models.CharField(choice = 'admin')
+class Admin(User):
+    user_type = 'admin'
+    def is_admin(self):
+        return self.user_type == models.CharField(choice = 'admin')
 
-    class Buyer:
-        def is_buyer(self):
-            return self.user_type == models.CharField(choice = 'buyer')
+class Buyer(User):
+    user_type = 'buyer'
+    def is_buyer(self):
+        return self.user_type == models.CharField(choice = 'buyer')
 
-    class Seller:
-        def is_seller(self):
-            return self.user_type == models.CharField(choice = 'seller')
+class Seller(User):
+    user_type = 'seller'
+    def is_seller(self):
+        return self.user_type == models.CharField(choice = 'seller')
     
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2,max_digits=10)
     
 class Cart(models.Model):
-    user = models.ForeignKey(User.Buyer)
-    product = models.ForeignKey(Product)
+    user = models.ForeignKey(Buyer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
 class Order(models.Model):
-    items = models.ForeignKey(Cart)
+    items = models.ForeignKey(Cart,on_delete=models.CASCADE)
