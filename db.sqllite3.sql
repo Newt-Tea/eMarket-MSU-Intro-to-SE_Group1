106a3c226ca600b5a6a0e605204a66644407f81c
Jeremiah Ickom <143756467+ji256@users.noreply.github.com>
2024-10-28 15:40:43 -0500

    Update db.sqllite3.sql
    I took out account balance and the related insert values

--- db.sqllite3.sql
+++ db.sqllite3.sql
@@ -18,18 +18,20 @@ CREATE TABLE IF NOT EXISTS "User" (
 	"Username"	varchar(10) NOT NULL UNIQUE,
 	"Password"	varchar(10) NOT NULL UNIQUE,
 	"Email"	varchar(45) NOT NULL UNIQUE,
-	"AccountBalance"	FLOAT (10,2),
 	PRIMARY KEY("UserId")
 );
-INSERT INTO "Product" ("ProductID","ProductName","Price","Stock") VALUES 
+INSERT INTO "Product" ("ProductID","ProductName","Price","Stock") 
+VALUES 
  (1544,'GI Joe Action Figure',26.1,9),
  (2662,'Lego Set',87.26,4),
  (3215,'Chips',2.69,20),
  (5986,'Bottled Water',1.17,45),
  (7412,'Graphic Tee',32.99,5),
  (7845,'Dog Collar',3.45,15);
-INSERT INTO "User" ("UserId","Username","Password","Email","AccountBalance") VALUES (1,'Pat_15','IlUv_Refs','3peat@gmail.com',2500.0),
- (2,'RL_52','Mans_Game','ravensLB@yahoo.com',2500.0),
- (3,'DAK_4','Romo=Dak','wedemboys@hotmail.com',2500.0),
- (4,'RG_3','@Gone2Soon','newknees@bing.com',2500.0);
+INSERT INTO "User" ("UserId","Username","Password","Email") 
+VALUES 
+ (1,'Pat_15','IlUv_Refs','3peat@gmail.com'),
+ (2,'RL_52','Mans_Game','ravensLB@yahoo.com'),
+ (3,'DAK_4','Romo=Dak','wedemboys@hotmail.com'),
+ (4,'RG_3','@Gone2Soon','newknees@bing.com');
 COMMIT;
