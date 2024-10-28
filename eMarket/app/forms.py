eab33332a267a05fd086f2d91a07b67a430c5ebf
Andrea Ambrose <andreamoesha@gmail.com>
2024-10-28 16:28:22 -0500

    Updated the Registration form
    Updated the Registration form with the correct choice variable "USER_TYPE_CHOICES"

--- eMarket/app/forms.py
+++ eMarket/app/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User, Product
 
 class RegistrationForm(UserCreationForm, ModelForm):
-    role = forms.ChoiceField(widget=forms.Select,choices=User.user_type)
+    role = forms.ChoiceField(widget=forms.Select,choices=User.USER_TYPE_CHOICES)
     class Meta:
         model = User
         fields = ['role', 'username', 'password1', 'password2']
