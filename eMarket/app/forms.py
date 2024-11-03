bb24730bbfe71d4cb5e0b737eea231a0396e5f3d
Hudson H <Hudsonhargrove01@gmail.com>
2024-11-03 17:23:35 +0000

    Update PaymentForm field lengths for expiry date and CVV

--- eMarket/app/forms.py
+++ eMarket/app/forms.py
@@ -19,5 +19,5 @@ class ProductCreationForm(ModelForm):
         
 class PaymentForm(forms.Form):
     card_number = forms.CharField(max_length = 16, label='Card Number')
-    expiry_date = forms.CharField(max_length = 10, label='Expiry Date (MM/YY)')
-    cvv = forms.CharField(label='CVV')
+    expiry_date = forms.CharField(max_length = 5, label='Expiry Date (MM/YY)')
+    cvv = forms.CharField(max_length = 3, label='CVV')
