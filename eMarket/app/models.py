ab36be7bb59fc182c7c838416f93bc12e1c9783f
LostSpaghetti <mccrosno@gmail.com>
2024-11-04 17:29:30 -0600

    Changed models and admin to display models

--- eMarket/app/models.py
+++ eMarket/app/models.py
@@ -16,21 +16,23 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=1)
     date_created = models.DateTimeField(null=True,auto_now_add=True)
     seller_username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, limit_choices_to={'user_type': 'seller'}, related_name='products')
-
     def __str__(self):
         return self.name
 
-
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'buyer'})
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
-    def get_total(self):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'buyer'})
+
+# Through model that defines the relationship between the Product and the Cart
+class CartProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) # connects to ONE product
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cartProducts') # connects to ONE cart
+    quantity = models.PositiveIntegerField(default=1) # describes the quantity of the connected product
+    def get_total(self): # gets total prices of connected product
         return self.quantity * self.product.price
-    
+
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'buyer'})
-    cart = models.ManyToManyField(Cart)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cart = models.OneToOneField(Cart, on_delete=models.SET_NULL, null=True)
     quantity = models.PositiveIntegerField(default=1)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     status = models.CharField(max_length = 50, default='Pending')
