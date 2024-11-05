68f9d6bd9a69cb6a5a513af7f78e7af5937dc1a5
Hudson H <Hudsonhargrove01@gmail.com>
2024-11-05 01:14:19 +0000

    Update ALLOWED_HOSTS to include 'newttea.pythonanywhere.com'

--- eMarket/eMarket/settings.py
+++ eMarket/eMarket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-imb&cpk2vc^)=3h(+7y@)+!26@ttjk+z)dzz5gr%75wfoa8p@i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newttea.pythonanywhere.com']
 
 
 # Application definition
