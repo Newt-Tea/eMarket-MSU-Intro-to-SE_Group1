b0687989c14e3cb9e7dce873ab1b628868a357cc
Andrea Ambrose <andreamoesha@gmail.com>
2024-11-11 15:53:51 -0600

    Added description field to Product creation form

--- eMarket/app/forms.py
+++ eMarket/app/forms.py
@@ -12,7 +12,7 @@ class RegistrationForm(UserCreationForm, ModelForm):
 class ProductCreationForm(ModelForm):
     class Meta:
         model = Product
-        fields = ['name','price','stock','image']
+        fields = ['name','price','description','stock','image']
         labels = {
             'name' : 'Item Name',
             'price' : 'Price $',
